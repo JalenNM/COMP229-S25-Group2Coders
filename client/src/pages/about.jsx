8077cae128f408f254e7b5ad44021cd7a12381d7
ab-fatxma <fabdi18@my.centennialcollege.ca>
2025-07-16 16:51:21 -0600

    updated about css

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react"
+import '../App.css';
 
 export default function About() {
 
     return (
-        <>
+        <div className="about-container">
             <h1>About Page</h1>
             <p>FlickBuzz is a web application that allows users to browse, view, and rate top-ranked movies. The site displays a list of movies with posters, ratings, and basic info.</p>
-        </>
+        </div>
     )
 }
