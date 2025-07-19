bbfa7e8f704b5a1a8ef7ab814eeb42699bcb5055
JalenNM <jalen.mendonca@gmail.com>
2025-07-19 14:45:33 -0400

    Navbar links centered/contact page moved to footer

--- client/src/components/Footer/footer.jsx
+++ client/src/components/Footer/footer.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import './footer.css'
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './footer.css';
 
 const Footer = () => {
     return (
-        
-<footer className="footer">
-<div className="container">
-  <p className="text-left">
-    &copy; {new Date().getFullYear()} Copyright <br>
-    </br>
-    Group 2 Coders. All rights reserved.
-  </p>
-</div>
-</footer>
+        <footer className="footer">
+            <div className="container">
+                <p className="text-left">
+                    &copy; {new Date().getFullYear()} Copyright <br />
+                    Group 2 Coders. All rights reserved.
+                </p>
+                <p className="text-left">
+                    <Link to="/contact" className="footer-link">Contact</Link>
+                </p>
+            </div>
+        </footer>
     )
 }
 
