33c0efbf6b0fa61c410a049430770254b19162b2
JalenNM <jalen.mendonca@gmail.com>
2025-07-19 16:10:44 -0400

    fixed footer size/static year 2025

--- client/src/components/Footer/footer.jsx
+++ client/src/components/Footer/footer.jsx
@@ -6,14 +6,16 @@ import './footer.css';
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="container">
-                <p className="text-left">
-                    &copy; {new Date().getFullYear()} Copyright <br />
-                    Group 2 Coders. All rights reserved.
-                </p>
-                <p className="text-left">
+            <div className="footer-content">
+                <div className="footer-left">
+                    <p>
+                        &copy; 2025 Copyright <br />
+                        Group 2 Coders. All rights reserved.
+                    </p>
+                </div>
+                <div className="footer-right">
                     <Link to="/contact" className="footer-link">Contact</Link>
-                </p>
+                </div>
             </div>
         </footer>
     )
